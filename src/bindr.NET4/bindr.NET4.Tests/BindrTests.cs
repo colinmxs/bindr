4fa188def29694557b1414ed0a1edd4611ad9314
colinsmith1 <colinsmith1@boisestate.edu>
2017-01-10 16:49:37 -0700

    added test foundation

---
+++ src/bindr.NET4/bindr.NET4.Tests/BindrTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Specialized;
+using Shouldly;
+
+namespace bindr.NET4.Tests
+{
+    public class BindrTests
+    {
+        private const string Id = "1";
+        private const string Value = "Howdy";
+        private readonly TestObject _result;
+
+        public BindrTests()
+        {
+            var bindr = new Bindr();
+            var @object = new NameValueCollection();
+            @object.Add("Id", Id);
+            @object.Add("Value", Value);
+            var testObject = new TestObject();
+            _result = bindr.Bind(@object, testObject);
+        }
+
+
+        public void VerbatimPropertyBindingInt()
+        {
+            _result.Id.ShouldBe(Id);
+        }
+
+        public void VerbatimPropertyBindingString()
+        {
+            _result.Value.ShouldBe(Value);
+        }
+
+        public class TestObject
+        {
+            public string Id { get; set; }
+            public string Value { get; set; }
+        }
+    }
+}
---
+++ src/bindr.NET4/bindr.NET4/Bindr.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Specialized;
+using System.Linq;
+
+namespace bindr.NET4
+{
+    public class Bindr
+    {
+        public T Bind<T>(NameValueCollection nvc, T model)
+        {
+            var keys = nvc.AllKeys;
+            var modelType = model.GetType();
+            var modelProps = modelType.GetProperties();
+            foreach (var key in keys)
+            {
+                if (modelProps.All(mp => mp.Name != key)) continue;
+                var value = nvc[key];
+                modelType.GetProperty(key).SetValue(model, value);
+            }
+            return model;
+        }
+    }
+}
