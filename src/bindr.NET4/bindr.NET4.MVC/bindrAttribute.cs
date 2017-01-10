43f33529fc80529ed97e02e9291a82f02f44bec8
colinsmith1 <colinsmith1@boisestate.edu>
2017-01-10 15:20:47 -0700

    added test project and framework dependencies

---
+++ src/bindr.NET4/bindr.NET4.MVC/bindrAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace bindr.NET4.MVC
+{
+    public class BindrAttribute : FilterAttribute, IActionFilter
+    {
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/bindr.NET4/bindr.NET4.WebAPI/BindrAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace bindr.NET4.WebAPI
+{
+    public class BindrAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
